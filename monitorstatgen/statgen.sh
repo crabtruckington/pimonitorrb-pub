d7c11031d44d6fee775d9e6a5b4a8d168ffc11c4
crab <nun@ya.bees.wax>
2020-08-24 01:11:18 -0400

    fixed .sh file for pis

--- monitorstatgen/statgen.sh
+++ monitorstatgen/statgen.sh
@@ -1,8 +1,8 @@
 iostat -c | sed '4q;d' | awk '{print "cpuuser=" $1 "\r\n" "cpusystem=" $3 "\r\n" "cpuidle=" $6 }' #cpuusage
-echo "cpuclockspeed=666"
-echo "cputemp=38.0"
+vcgencmd measure_clock arm  | awk -F "[=\']" '{print "cpuclockspeed=" $2/1000000}' #cpu speed
+vcgencmd measure_temp  | awk -F "[=\']" '{print "cputemp="$2}' #temperature
 cat /proc/meminfo | sed '3q' | awk '{print $1 $2}' | sed 's/:/=/' #memoryusage
-df | grep "/dev/sda1" | awk '{print "drivetotalmb=" $2/1024 "\r\ndriveusedmb=" $3/1024 "\r\ndriveavailablemb=" $4/1024}' #disk usage
+df | grep "root" | awk '{print "drivetotalmb=" $2/1024 "\r\ndriveusedmb=" $3/1024 "\r\ndriveavailablemb=" $4/1024 }' #disk usage
 iostat -d | grep "sda" | awk '{print "drivekbreads="$3"\r\ndrivekbwrites="$4 }' #disk IO stats
-ifstat -i enp3s0  0.1 1 | sed '3q;d' | awk '{print "networkkbin=" $1 "\r\nnetworkkbout=" $2}' #network usage
+ifstat -i eth0  0.1 1 | sed '3q;d' | awk '{print "networkkbin=" $1 "\r\nnetworkkbout=" $2}' #network usage
 uptime -s | awk '{print "systemuptime="$1, $2}'
