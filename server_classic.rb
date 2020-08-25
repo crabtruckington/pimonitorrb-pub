2a2f0420132d8aaec37d0332acfe98d2cfaaec6b
crab <nun@ya.bees.wax>
2020-08-24 23:59:43 -0400

    rewrote stat and page generation to use an aggregate of stats collected more often. This gives a better idea of performance over time, while still allowing the graphs to be readable. 'classic' generation files are available if you do not want to generate this much load, as its kind of expensive

---
+++ server_classic.rb
@@ -0,0 +1,58 @@
+require 'socket'
+require 'uri'
+require_relative "serveRequest"
+require_relative "logging"
+require_relative "statsgen"
+
+# base directory
+WEB_ROOT = './content'
+
+def sanitizeUserRequests(requestContent, ipAddr)
+    request_uri  = requestContent.split(" ")[1]
+    path = URI.unescape(URI(request_uri).path)  
+    clean = []
+  
+    parts = path.split("/")
+  
+    loggedDangerousRequest = 0
+    parts.each do |part|
+      if part.empty? 
+        #do nothing
+      elsif part == "." || part == ".." || part == "..."
+        
+      #elsif part == ".."
+        #clean.pop   #im pretty sure I dont want to support this shit
+        if loggedDangerousRequest == 0
+          Log.log("User at #{ipAddr} made dangerous request: #{requestContent} , it was sanitized")
+          loggedDangerousRequest = 1
+        end
+      else
+        clean << part  
+      end
+
+      #next if part.empty? || part == '.'
+      #part == '..' ? clean.pop : clean << part
+    end
+
+    File.join(WEB_ROOT, *clean)
+end
+
+t1 = Thread.new do
+  while true do
+    StatsGen.statsGenThread()
+    Log.log("Garbage collecting", 0)
+    GC.start()        
+  end
+end
+
+server = TCPServer.new('localhost', 6689)
+
+loop do
+  #socket = server.accept
+  Thread.start(server.accept) do |socket|  
+    requestContent = socket.gets    
+    path = sanitizeUserRequests(requestContent, socket.peeraddr)
+    ServeRequest.serveRequest(socket, path)
+  end
+end
+
